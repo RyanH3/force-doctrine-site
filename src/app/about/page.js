d6587ea6e709b871c3e0f6581da14219aa9c7386
RyanH3 <ryanhwilliams2@gmail.com>
2025-09-05 19:10:09 -0400

    Added pages with routes

---
+++ src/app/about/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>About the Instructor</h1>
+    </main>
+  );
+}
---
+++ src/app/secret-techniques/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>Secret Techniques catalog</h1>
+    </main>
+  );
+}
