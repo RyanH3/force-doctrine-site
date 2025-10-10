ae044f0352f556e61b116d3436c268d918b4390a
RyanH3 <ryanhwilliams2@gmail.com>
2025-10-10 16:49:50 -0400

    Removed redundant supabase utility

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -35,7 +35,7 @@ export default async function Page() {
   return (
     <main className="flex font-sans bg-black text-yellow-400 place-content-center">
       <form className="grid grid-cols-1 grid-rows-7 items-end mt-[24vw] md:max-w-[20vw] md:mt-[6vw]" action={submitForm}>
-        <label htmlFor="username">Username:</label>
+        <label htmlFor="username">Name:</label>
         <input className="mb-[1vw] outline outline-white rounded-lg text-gray-400" id="username" type="text" name="username"/>
         <label htmlFor="email">Email address:</label>
         <input className="mb-[1vw] outline outline-white rounded-lg text-gray-400" id="email" type="email" name="email" required/>
